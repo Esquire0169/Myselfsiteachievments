f428ba7b1169e5c429ea88e480af047e44dc056e
Esquire0169 <diman3332013@gmail.com>
2026-04-09 20:45:30 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if (burger && mobileMenu) {
+  burger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open");
+  });
+
+  mobileMenu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.remove("open");
+    });
+  });
+}
